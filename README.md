b6e5f2f6e085417a5e71ea441e2bec94ff58f626
U <u@fiat2.co>
2023-03-06 21:23:22 +0800

    Included the official example within the app for demonstration purposes.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # docx_template_flutter_sample
 A sample Flutter app that demonstrate how to use the `docx_template` package to generate Word documents from templates.
+
+## Note
+
+### Android
+
+`android/app/src/main/AndroidManifest.xml`
+
+```xml
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.example.docx_template_flutter_sample">
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+    <application
+        android:requestLegacyExternalStorage="true"
+...
+```
+
+To write Word documents in the public Download directory, which is `/storage/emulated/0/Download/`, of Android phones, it is necessary to include `android:requestLegacyExternalStorage="true"` in the app's manifest file.
+
+## References
+
+- [docx_template 0.3.3](https://pub.dev/packages/docx_template)
+- [docx_template_dart](https://github.com/PavelS0/docx_template_dart)
+- [Word file gets corrupted](https://github.com/PavelS0/docx_template_dart/issues/37)
+- [Modify the App Manifest (androidManifest.xml)](https://www.ibm.com/docs/en/trusteer-mobile-sdk/5.2?topic=qsta-step-1-modify-app-manifest-androidmanifestxml)
+- [Icons8](https://icons8.com/license)
---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:docx_template/docx_template.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Docx Template Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('docx_template Flutter Sample'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed: _generateDocument,
+              child: const Text('Generate Document (Official Example)'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _generateDocument() async {
+    final status = await Permission.storage.request();
+    if (!status.isGranted) {
+      print('Permission denied');
+      return;
+    }
+
+    final data = await rootBundle.load('assets/template.docx');
+    final bytes = data.buffer.asUint8List();
+    final docx = await DocxTemplate.fromBytes(bytes);
+
+    // Load test image for inserting in docx
+    // final testFileContent = await File('assets/test.png').readAsBytes();
+    final testFileData = await rootBundle.load('assets/test.png');
+    final testFileContent = testFileData.buffer.asUint8List();
+
+    final listNormal = ['Foo', 'Bar', 'Baz'];
+    final listBold = ['ooF', 'raB', 'zaB'];
+
+    final contentList = <Content>[];
+
+    final b = listBold.iterator;
+    for (var n in listNormal) {
+      b.moveNext();
+
+      final c = PlainContent("value")
+        ..add(TextContent("normal", n))
+        ..add(TextContent("bold", b.current));
+      contentList.add(c);
+    }
+
+    Content c = Content();
+    c
+      ..add(TextContent("docname", "Simple docname"))
+      ..add(TextContent("passport", "Passport NE0323 4456673"))
+      ..add(TableContent("table", [
+        RowContent()
+          ..add(TextContent("key1", "Paul"))
+          ..add(TextContent("key2", "Viberg"))
+          ..add(TextContent("key3", "Engineer"))
+          ..add(ImageContent('img', testFileContent)),
+        RowContent()
+          ..add(TextContent("key1", "Alex"))
+          ..add(TextContent("key2", "Houser"))
+          ..add(TextContent("key3", "CEO & Founder"))
+          ..add(ListContent("tablelist", [
+            TextContent("value", "Mercedes-Benz C-Class S205"),
+            TextContent("value", "Lexus LX 570")
+          ]))
+          ..add(ImageContent('img', testFileContent))
+      ]))
+      ..add(ListContent("list", [
+        TextContent("value", "Engine")
+          ..add(ListContent("listnested", contentList)),
+        TextContent("value", "Gearbox"),
+        TextContent("value", "Chassis")
+      ]))
+      ..add(ListContent("plainlist", [
+        PlainContent("plainview")
+          ..add(TableContent("table", [
+            RowContent()
+              ..add(TextContent("key1", "Paul"))
+              ..add(TextContent("key2", "Viberg"))
+              ..add(TextContent("key3", "Engineer")),
+            RowContent()
+              ..add(TextContent("key1", "Alex"))
+              ..add(TextContent("key2", "Houser"))
+              ..add(TextContent("key3", "CEO & Founder"))
+              ..add(ListContent("tablelist", [
+                TextContent("value", "Mercedes-Benz C-Class S205"),
+                TextContent("value", "Lexus LX 570")
+              ]))
+          ])),
+        PlainContent("plainview")
+          ..add(TableContent("table", [
+            RowContent()
+              ..add(TextContent("key1", "Nathan"))
+              ..add(TextContent("key2", "Anceaux"))
+              ..add(TextContent("key3", "Music artist"))
+              ..add(ListContent(
+                  "tablelist", [TextContent("value", "Peugeot 508")])),
+            RowContent()
+              ..add(TextContent("key1", "Louis"))
+              ..add(TextContent("key2", "Houplain"))
+              ..add(TextContent("key3", "Music artist"))
+              ..add(ListContent("tablelist", [
+                TextContent("value", "Range Rover Velar"),
+                TextContent("value", "Lada Vesta SW Sport")
+              ]))
+          ])),
+      ]))
+      ..add(ListContent("multilineList", [
+        PlainContent("multilinePlain")
+          ..add(TextContent('multilineText', 'line 1')),
+        PlainContent("multilinePlain")
+          ..add(TextContent('multilineText', 'line 2')),
+        PlainContent("multilinePlain")
+          ..add(TextContent('multilineText', 'line 3'))
+      ]))
+      ..add(TextContent('multilineText2', 'line 1\nline 2\n line 3'))
+      ..add(ImageContent('img', testFileContent));
+
+    final d = await docx.generate(c);
+    const filepath = '/storage/emulated/0/Download/generated_306.docx';
+    final of = File(filepath);
+    if (d != null) {
+      await of.writeAsBytes(d);
+      Fluttertoast.showToast(
+          msg: 'Document $filepath generated!', toastLength: Toast.LENGTH_LONG);
+    }
+  }
+}
